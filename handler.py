b799a2f29fdd6b5fed943d64b9a7ba6250935b0c
yu-ku <yuku08@gmail.com>
2017-12-23 18:30:45 +0900

    [add] handlerにget_balanceメソッドを追加

--- handler.py
+++ handler.py
@@ -1,24 +1,28 @@
 import json
+import os
+import requests
+import logging
+from cc_privateAPI import Coincheck
 
+CC_ACCESS_KEY = os.environ['CC_ACCESS_KEY']
+CC_SECRET_KEY = os.environ['CC_SECRET_KEY']
+
+logger = logging.getLogger()
 
 def loktarogar(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+    coincheck = Coincheck(CC_ACCESS_KEY, CC_SECRET_KEY)
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+    try:
+        balance_json = get_balance(coincheck)
+        balance_jpy = balance_json['jpy']
+        balance_btc = balance_json['btc']
+    except Exception as e:
+        logger.error(e)
+        return 'get_balance error!'
 
-    return response
+    return balance_json
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+def get_balance(coincheck):
+    path_balance = '/api/accounts/balance'
+    result = coincheck.get(path_balance)
+    return result
